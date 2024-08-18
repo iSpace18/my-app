5f30e89ee77e59afe01c90580fdd484a876c4850
ispace18 <steamdavid2007@gmail.com>
2024-08-18 18:19:56 +0300

    13541345

--- src/App.js
+++ src/App.js
@@ -6,39 +6,38 @@ function App() {
       <header className="header">
         <div className="logo">MAKEUPDATE</div>
         <nav className="nav">
-          <a href="#">Главная</a>
-          <a href="#">Уроки</a>
-          <a href="#">Пользователи</a>
-          <a href="#">Сообщения</a>
+          <a href="#"><img src= 'https://cdn.builder.io/api/v1/image/assets/TEMP/b126e677546e02aa20f1fdb957ad3ac26dadb0b2f17a3d62fceb685a73d46b30?placeholderIfAbsent=true&apiKey=26bd9a38384c4232b7f8df003fc57b5b'></img>  Главная</a>
+          <a href="#"> <img className='vxbxbvc' src='https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f'></img>   Уроки</a>
+          <a href="#"> <img className='qryt' src='https://cdn.builder.io/api/v1/image/assets/TEMP/7a6737baa52b6ba2a8cb4622713f564090ae4d367371b851819e5879c61eda62?placeholderIfAbsent=true&apiKey=26bd9a38384c4232b7f8df003fc57b5b'></img> Пользователи</a>
+          <a href="#"> <img src='https://cdn.builder.io/api/v1/image/assets/TEMP/628616b5526dcf2715ea838ecf261381b02b2174a18a027452fbca9a9e30df26?placeholderIfAbsent=true&apiKey=26bd9a38384c4232b7f8df003fc57b5b'></img> Сообщения</a>
         </nav>
         <nav className="nav2">
           <a href="#">
-            <img src="group 82.png" alt="Иконка 1" />
+            <img className='proffoto' src="https://cdn.builder.io/api/v1/image/assets/TEMP/46d1f3956fe11ec7ecbee35ab7d50f0af93879be722fea0e4326d837bc939a12?placeholderIfAbsent=true&apiKey=26bd9a38384c4232b7f8df003fc57b5b" alt="Иконка 1" />
           </a>
           <a href="#">
-            <img src="Group 83.png" alt="Иконка 2" />
+            <img className='qwe12' src="https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F82b8cb03c7744c04860b0617f6951d77" alt="Иконка 2" />
           </a>
           <a href="#">
-            <img src="Group 84.png" alt="Иконка 3" />
+            <img src='https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F1fd5a3158f2a46d899c81e70b3a7dd47' alt="Иконка 3" />
           </a>
         </nav>
       </header>
 
       <div className="profile-container">
         <div className="profile-card">
-          <img
+          <img className='nnnn' src='https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F0ac6c0f83a464a87be0182025ee9deb1'></img>
+          <img className='chel'
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/cc9acba689f81ba9d2ecbc8f521b9ce65f76070b0a958433d0bbed78957a5941?placeholderIfAbsent=true&apiKey=26bd9a38384c4232b7f8df003fc57b5b"
             alt="Profile Picture"
           />
           <div className="info">
-            <p>Россия, Сочи</p>
-            <p>
-              <span>
-                <img className="BEBE" src="image.png" alt="Иконка возраста" />
-              </span>
-              32
-            </p>
-            <p>100 203 подписчиков</p>
+            <p className='adf'>     Россия, Сочи</p>   
+            <img className='mayak' src='https://cdn.builder.io/api/v1/image/assets/TEMP/400663f641c642488465e118aec09e05f410b9eb6133ea4d51572e5c0eeb7fd7?placeholderIfAbsent=true&apiKey=26bd9a38384c4232b7f8df003fc57b5b'></img>
+          <img className='rrr' src='https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F6d764420350c4cb8b3c81a3d21a6172f'></img>
+          <p className='ppp'>32</p>
+          <img className='vbvb' src='https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F26bd9a38384c4232b7f8df003fc57b5b%2F9ac2a06071a349d3864dda0e468b50f0'></img>
+        <p className='z123zxc'>100 203</p>
           </div>
           <button className="btnrgb">Редактировать</button>
           <button className="btn">Опубликовать урок</button>
@@ -56,7 +55,7 @@ function App() {
         </div>
       </div>
 
-      <div className="tabs">
+      <div className="tabs2">
         <button>Мои уроки</button>
         <button>Доступ к урокам</button>
         <button>Избранные уроки</button>
